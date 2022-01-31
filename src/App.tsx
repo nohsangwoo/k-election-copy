f9d1b64f5737f77c57d096ec42d56d6307df603c
nohsangwoo <nsgr12@naver.com>
2022-01-31 23:30:02 +0900

    init settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
+import Router from 'router'
+import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from 'styles/theme'
+import { useRecoilValue } from 'recoil'
+import { darkModeState } from 'atoms/globalStylesAtom'
+import { GlobalStyles } from 'styles/globalStyles'
 
 function App() {
-  return <div className="App">App</div>;
+  const isDarkMode = useRecoilValue(darkModeState)
+
+  return (
+    <>
+      <HelmetProvider>
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router />
+        </ThemeProvider>
+      </HelmetProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
